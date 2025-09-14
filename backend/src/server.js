dee6671aed8df5fd1bdafcf29b3a2faabfe14e4d
khang tran <qkhang1545678@gmail.com>
2025-09-14 13:57:02 +0700

    9/14

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,22 +4,32 @@ import taskRoutes from "./routes/taskRoutes.js"; // Các routes xử lý cho "ta
 import { connectDB } from "./config/db.js"; // Hàm kết nối Database (MongoDB, v.v.)
 import dotenv from "dotenv"; // Thư viện để đọc file .env
 import cors from "cors"; // Thư viện để cho phép truy cập từ các domain khác
+import path from "path";
 // Load biến môi trường từ file .env vào process.env
 dotenv.config();
 
 // Lấy PORT từ file .env, nếu không có thì mặc định là 5001
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve(); // Lấy đường dẫn thư mục hiện tại
 
 // Khởi tạo ứng dụng Express
 const app = express();
 
 // Middleware: cho phép Express đọc dữ liệu JSON từ request body
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:5173" })); // Cho phép truy cập từ các domain khác
+if (process.env.NODE_ENV === "development") {
+  app.use(cors({ origin: "http://localhost:5173" })); // Cho phép truy cập từ các domain khác
+}
 
 // Gắn các routes cho endpoint "/api/tasks"
 // Khi gọi http://localhost:5001/api/tasks/... thì sẽ chạy vào taskRoutes
 app.use("/api/tasks", taskRoutes);
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 // Kết nối database trước, nếu thành công thì mới chạy server
 connectDB().then(() => {
